8b2f9ea090fcad00334b8c9bbb7644ee6d41412a
Kenan Shifflett <kenan.shifflett@gmail.com>
2014-06-24 22:54:39 -0400

    Update example.js

--- example.js
+++ example.js
@@ -4,33 +4,25 @@ var client_secret=process.env.MSCLIENT_SECRET;
 var client_id=process.env.MSCLIENT_ID;
 
 if (!client_secret || !client_id) {
-  console.log("client_secret and client_id missing");
+  console.log('client_secret and client_id missing');
   process.exit(1);
 }
 
-var params = { 
-  text: 'How\'s it going?'
-  , from: 'en'
-  , to: 'es'
+var params = {
+  text: 'How\'s it going?',
+  from: 'en',
+  to: 'es'
 };
 
-var client = new MsTranslator({client_id: client_id, client_secret: client_secret});
+var client = new MsTranslator({
+  client_id: client_id,
+  client_secret: client_secret
+});
 
 client.initialize_token(function(){
   client.translate(params, function(err, data) {
     if (err) console.log('error:' + err);
     console.log(data);
-  });
-  client.getLanguagesForTranslate(function(err, data) {
-    if (err) console.log('error:' + err);
-    console.log(data);
-    var options = {
-      locale: 'en',
-      languageCodes: data
-    };
-    client.getLanguageNames(options, function(err, data) {
-      if (err) console.log('error:' + err);
-      console.log(data);
-    });
+    process.exit();
   });
 });
